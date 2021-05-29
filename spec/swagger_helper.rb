ece60a16d12d39d34a49afca0401767731ddbae5
jesus martinez <jesus.alberto.vk@gmail.com>
2021-05-28 19:52:43 -0500

    Integrate swagger documentation

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.configure do |config|
   # Specify a root folder where Swagger JSON files are generated
   # NOTE: If you're using the rswag-api to serve API descriptions, you'll need
   # to ensure that it's configured to serve Swagger from the same folder
-  config.swagger_root = Rails.root.join('swagger').to_s
+  config.swagger_root = Rails.root.join("swagger").to_s
 
   # Define one or more Swagger documents and provide global metadata for each one
   # When you run the 'rswag:specs:swaggerize' rake task, the complete Swagger will
@@ -15,43 +15,64 @@ RSpec.configure do |config|
   # document below. You can override this behavior by adding a swagger_doc tag to the
   # the root example_group in your specs, e.g. describe '...', swagger_doc: 'v2/swagger.json'
   config.swagger_docs = {
-    'v1/swagger.yaml' => {
-      openapi: '3.0.1',
+    "v1/swagger.yaml" => {
+      openapi: "3.0.1",
       info: {
-        title: 'API V1',
-        version: 'v1'
+        title: "API V1",
+        version: "v1",
+      },
+      components: {
+        schemas: {
+          user: {
+            type: "object",
+            properties: {
+              email: { type: "string", required: true },
+              password: { type: "string", required: true },
+              organization_attributes: {
+                type: "object",
+                properties: {
+                  name: { type: "string", required: true },
+                },
+              },
+            },
+            example: {
+              user: {
+                email: "test@test.com",
+                password: "123456",
+                organization_attributes: {
+                  name: "test",
+                },
+              },
+            },
+          },
+          user_login: {
+            type: "object",
+            properties: {
+              email: { type: "string", required: true },
+              password: { type: "string", required: true },
+            },
+            example: {
+              user: {
+                email: "test@test.com",
+                password: "123456",
+              },
+            },
+          },
+        },
       },
       paths: {},
       servers: [
         {
-          url: 'https://{defaultHost}',
+          url: "http://{defaultHost}",
           variables: {
             defaultHost: {
-              default: 'localhost:4002'
-            }
-          }
-        }
-      ]
+              default: "localhost:4000",
+            },
+          },
+        },
+      ],
     },
 
-    'v2/swagger.yaml' => {
-      openapi: '3.0.1',
-      info: {
-        title: 'API V2',
-        version: 'v2',
-        description: 'This is the second version of my API'
-      },
-      servers: [
-        {
-          url: 'https://{defaultHost}',
-          variables: {
-            defaultHost: {
-                default: 'www.example.com'
-            }
-          }
-        }
-      ]
-    }
   }
 
   # Specify the format of the output Swagger file when running 'rswag:specs:swaggerize'.
